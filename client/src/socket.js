37b74657550fc63c5f6d20820c0fd27f7fd3e65d
Will Taylor <taylorw@udel.edu>
2019-12-14 14:38:48 -0500

    Fix socket io socket

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,6 +1,6 @@
 import io from 'socket.io-client';
 
-export const socket = io('http://localhost:3001');
+export const socket = io();
 
 export const subscribeToUserUpdate = (cb) => {
   socket.on('user update', (data) => {
